75f5e89bc8d87b3054bc8f431b05c3db8d6c00d2
Julio Elias <julio.elias22@gmail.com>
2025-06-28 23:32:08 +0300

    Update works.jsx

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -10,7 +10,7 @@ const Works = () => {
                 <div className="works">
                         <Card
                                 icon={faBriefcase}
-                                title="Work"
+                                title="CV"
                                 body={
                                         <div className="works-body">
                                                <div className="cv-qr-wrapper">
