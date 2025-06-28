097efbb07f97c40ab09e2493f3028c9069b146c1
Julio Elias <julio.elias22@gmail.com>
2025-06-29 01:12:50 +0300

    Update cv.jsx

--- src/components/homepage/cv.jsx
+++ src/components/homepage/cv.jsx
@@ -1,26 +1,24 @@
 // src/components/CV.jsx
 import React from "react";
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
-import Card from "../common/card";          // keep your existing Card wrapper
-import "./styles/cv.css";                  // reuse your stylesheet
+import Card from "../common/card";
+import "./styles/cv.css";
 
-const CV = () => {
-  return (
-    <div className="cv">
-      <Card
-        icon={faBriefcase}
-        title="CV"
-        body={
-          <img
-            src={"MY_CV.png"} // replace with your CV image path
-            onClick={() => window.open("../MY_CV.png", "_blank")}
-            alt="Scan to download my CV"
-            className="cv-qr"
-          />
-        }
-      />
-    </div>
-  );
-};
+const CV = () => (
+  <div className="cv">
+    <Card
+      icon={faBriefcase}
+      title="CV"
+      body={
+        <img
+          src="/MY_CV.png"                          // <-- absolute path
+          onClick={() => window.open("/MY_CV.png", "_blank")}
+          alt="Scan to download my CV"
+          className="cv-qr"
+        />
+      }
+    />
+  </div>
+);
 
 export default CV;
