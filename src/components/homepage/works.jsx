34c859ef3eb395081ead2dd77fac7427d622c770
julio1703 <julio.elias22@gmail.com>
2025-06-28 23:23:18 +0300

    Link QR code to CV PDF

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -6,43 +6,26 @@ import Card from "../common/card";
 import "./styles/works.css";
 
 const Works = () => {
-	return (
-		<div className="works">
-			<Card
-				icon={faBriefcase}
-				title="Work"
-				body={
-					<div className="works-body">
-						<div className="work">
-							<img
-								src="./facebook.png"
-								alt="facebook"
-								className="work-image"
-							/>
-							<div className="work-title">Facebook</div>
-							<div className="work-subtitle">
-								Software Engineer
-							</div>
-							<div className="work-duration">2019 - Present</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./twitter.png"
-								alt="twitter"
-								className="work-image"
-							/>
-							<div className="work-title">Twitter</div>
-							<div className="work-subtitle">
-								Software Engineer
-							</div>
-							<div className="work-duration">2019 - Present</div>
-						</div>
-					</div>
-				}
-			/>
-		</div>
-	);
+        return (
+                <div className="works">
+                        <Card
+                                icon={faBriefcase}
+                                title="Work"
+                                body={
+                                        <div className="works-body">
+                                               <div className="cv-qr-wrapper">
+                                                       <img
+                                                               src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=%2Fcv.pdf"
+                                                               alt="CV QR Code"
+                                                               className="cv-qr"
+                                                       />
+                                                       <div className="cv-qr-text">Scan to view my CV</div>
+                                               </div>
+                                        </div>
+                                }
+                        />
+                </div>
+        );
 };
 
 export default Works;
