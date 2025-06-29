07126399d0734897d2f9193d746b353dfadd599b
julio1703 <julio.elias22@gmail.com>
2025-06-29 04:42:39 +0300

    Reset project locks each tab

--- src/pages/experiences.jsx
+++ src/pages/experiences.jsx
@@ -13,10 +13,10 @@ import myExperiences from "../data/experiences";
 import "./styles/experiences.css";
 
 const Experiences = () => {
-        useEffect(() => {
-                window.scrollTo(0, 0);
-                localStorage.setItem("visitedExperiences", "true");
-        }, []);
+       useEffect(() => {
+               window.scrollTo(0, 0);
+               sessionStorage.setItem("visitedExperiences", "true");
+       }, []);
 
 	const currentSEO = SEO.find((item) => item.page === "experiences");
 
